c61bef2a37f6db275cabab514db99f6f682a3088
wangruilong <862452159@qq.com>
2017-07-18 17:10:32 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,25 @@
+# new-project
+
+> A React project
+
+## Usage
+
+```bash
+# install all this dependencies.
+npm install
+
+# development, default port: 8080
+npm run dev
+
+# production
+npm run build
+
+# build with report
+npm run build --report
+
+# lint the files
+npm run lint
+
+# run the tests
+npm test
+```
---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+import MainView from 'views/MainView'
+import SyncView from 'views/SyncView'
+import Loadable from 'react-loadable'
+
+export default [
+  {
+    path: '/',
+    component: MainView,
+    childRoutes: [
+      {
+        path: '/sync',
+        component: SyncView
+      },
+      {
+        path: '/async',
+        component: Loadable({
+          loader: () => import(`views/AsyncView`),
+          // if you have your own loading component,
+          // you should consider add it here
+          LoadingComponent: () => null
+        })
+      }
+    ]
+  }
+]
