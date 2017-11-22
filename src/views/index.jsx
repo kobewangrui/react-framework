25e9bd8b007d71600a2714b94a6c538e07576583
wangruilong <kobewangrui@163.com>
2017-11-22 15:03:31 +0800

    在组件内redux state以及添加一些注释

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import { renderRoutes } from 'react-router-config' /**子路由**/
 import { NavLink as Link } from 'react-router-dom' /**路由跳转**/
-import Counter from 'components/Counter'
-
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'//引入react-redux
+import { bindActionCreators } from 'redux'//引入redux
+import * as NumberActions from '$redux/actions/number'
 import 'assets/css/index.css'
 
-export default class Index extends Component {
+class Index extends Component {
+  static propTypes = {
+    actions: PropTypes.object,//传入参数的类型
+    counter: PropTypes.object//传入参数的类型
+  }
   componentDidMount(){
-        console.log(renderRoutes(this.props.route.childRoutes));
+      this.props.actions.numberadd();//自执行的改变state的方法
+        // console.log(renderRoutes(this.props.route.childRoutes));
+        // console.log(connect);
+        // console.log(bindActionCreators);
+  }
+  changeNumber = ()=>{
+    this.props.actions.numberadd();
   }
   render(){
     return (
@@ -38,15 +50,52 @@ export default class Index extends Component {
             <p>
                 <Link to='/'>去首页</Link>
             </p>
-            {JSON.stringify(this.props.route.childRoutes)}
+            {/* {JSON.stringify(this.props.route.childRoutes)} */}
             {renderRoutes(this.props.route.childRoutes)}
           </div>        
         <div>
-          <Counter/>
           index footer
+          {JSON.stringify(this.props.number)}
+          <button onClick={this.changeNumber}>改变changechangechangechange</button>
+          ---Number:{this.props.number.number}---
         </div>
-
       </div>
     )
   }
-}
+};
+
+const mapStateToProps = state => ({//从redux中取出number，把state.number绑定到props的number
+  number: state.number
+});
+
+const mapDispatchToProps = (dispatch)=>({//将actions中的方法作为props绑定到组件
+  actions: bindActionCreators(NumberActions,dispatch)
+  //bindActionCreators：通过dispatch将action包裹起来，这样可以通过bindActionCreators创建的方法，直接调用dispatch(action) (隐式调用)
+  //bindActionCreators(oldActionCreator,dispatch)
+  // 参数解析 
+  // 形参：oldActionCreator这个参数就是创建的action的集合
+  // 形参：dispatch这里的dispatch,等同于store中的store.dispatch，用于组合action
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Index)//这里的Index就是当前组件
+
+// connect方法介绍 作用是连接组件与redux store
+// connect([mapStateToProps],[MapDispatchToProps],[mergeProps],[options])
+
+// mapStateToProps(state,ownProps)     :stateProps
+// const mapStateTopProps = (state) =>({count:state.count});
+// 这个函数的第一个参数就是redux的store我们从中摘取count属性。不必传入state的所有属性，取出需要的就行（如：state.count）；
+// 第二个参数ownProps,是组件自己的props
+
+
+// connect的第二个参数是mapDispatchToProps,功能是将action为props绑定到组件上，也会成为MyComp的props
+
+// [mergeProps]
+// 不管是stateProps还是dispatchProps,都需要和wonProps merge之后才会被赋给组件。connect的第三个参数就是用来做这件事。通常情况下，你可以不传参数，connect就会使用Object.assign替代该方法
+
+
+// [options](Object)如果指定这个参数，可以定制connector的行为。一般不用。
+
