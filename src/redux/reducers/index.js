b06ea12cc2dc8ddcd05a397d452ae6d584f688ac
wangruilong <kobewangrui@163.com>
2017-11-22 15:04:40 +0800

    在reducer返回number

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux'
 import counter from './counter'
+import number from './number'
 
 export default combineReducers({
-  counter
+  counter,
+  number
 })
+//combineReducers方法介绍:用于Reducer的拆分，你只要定义各个子Reducer函数，然后用这个方法，将他们合并成一个大的Reducer
