c2d146e3c8e38db7e4742876bf64f385aa9114db
wangruilong <kobewangrui@163.com>
2017-11-22 18:26:08 +0800

    关于两种state{}写法的一点感悟

--- src/views/SyncView.jsx
+++ src/views/SyncView.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
 
 export default class SyncView extends Component {
-  state = {
+  state = {//没有constructor可以这么写，根据情况而定（暂时这么理解）
     name: 'Sync'
+  }
+  // 两种写法而已constructor中必须有super()，而且必须用this.state{}
+  // constructor(){
+  //     super()
+  //     this.state = {
+  //       name : 'aSync'
+  //     }
+  //   }
+  componentWillMount(){
+    console.log(this.state);
   }
   render () {
     return (
