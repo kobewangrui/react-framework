04d7cb5f6ab234c0563d8940fa66bb8f24f19397
wangruilong <kobewangrui@163.com>
2017-11-22 17:25:34 +0800

    fetch获取用户数据，并且actions传参

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,46 @@ import React, { Component } from 'react'
 import { renderRoutes } from 'react-router-config'
 import { BrowserRouter as Router } from 'react-router-dom'
 import routes from 'routes'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'//引入react-redux
+import { bindActionCreators } from 'redux'//引入redux
+import * as NumberActions from '$redux/actions/number'
 
-export default class App extends Component {
+class App extends Component {
+  static propTypes = {
+    actions: PropTypes.object,
+    counter: PropTypes.object
+  }
+    componentWillMount(){
+      this.fetchData();
+    }
+
+  changeNumber = (msg)=>{
+    this.props.actions.numberadd(msg);
+  }
+    fetchData = ()=>{
+      let url = `/api/consumers/login`,
+          req = JSON.stringify({
+              userName : 'wrl',
+              passWord : '123123',
+          });
+      fetch(url,{
+              method:'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: req
+          })
+          .then((json)=>{
+              return json.json();
+          })
+          .then((res)=>{
+            this.changeNumber(res.msg);
+          }) 
+          .catch((err)=>{
+              console.log(err)
+          })
+    }
   render () {
     return (
       <div>
@@ -14,3 +52,16 @@ export default class App extends Component {
     )
   }
 }
+
+const mapStateToProps = state => ({
+  number: state.number
+});
+
+const mapDispatchToProps = (dispatch)=>({
+  actions: bindActionCreators(NumberActions,dispatch)
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
