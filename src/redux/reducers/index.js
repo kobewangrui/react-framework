7bc30a2a7f8e64c8974a8ffdeacd1fa95b046ad9
wangruilong <kobewangrui@163.com>
2017-11-24 17:55:47 +0800

    将异步state合并到reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import counter from './counter'
 import number from './number'
+import list_data from './async_list_data'
 
 export default combineReducers({
   counter,
-  number
+  number,
+  list_data
 })
 //combineReducers方法介绍:用于Reducer的拆分，你只要定义各个子Reducer函数，然后用这个方法，将他们合并成一个大的Reducer
